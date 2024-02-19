4b21e0feaf2102e7e5cd7d123b45870c4fb6b514
Panhakonpapa <linuxdev238@gmail.com>
2024-02-19 11:58:30 +0700

    build update

--- Build.sh
+++ Build.sh
@@ -1,3 +1,8 @@
 #!bin/bash 
-
-g++ main.cpp -o pong -lSDL2 -I./src ./src/pug1.h ./src/pong.h ./src/pug2.h  
+COMPILER="g++"
+MAIN="main.cpp"
+OUTPUTFILE="pong"
+LINKER="-lSDL2"
+# Compling Compile Compile 
+$COMPILER $MAIN -o $OUTPUTFILE $LINKER -I./src ./src/pug1.h ./src/pong.h ./src/pug2.h 
+# g++ main.cpp -o pong -lSDL2 -I./src ./src/pug1.h ./src/pong.h ./src/pug2.h  
